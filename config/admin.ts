005e163b5d2176052e9368386ba9373100c1bf0d
MrDiscoBoi <NaphatJm@gmail.com>
2023-02-21 23:15:15 +0700

    link data

--- config/admin.ts
+++ config/admin.ts
@@ -3,6 +3,6 @@ export default ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'B3NxIGxPZMqQ5JVQ1dhK8w=='),
   },
 });
