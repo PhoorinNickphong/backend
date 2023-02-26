4443133362ea32559c22bd4ec1360fc65cd3780d
Synai Sunthorn <6510110492@psu.ac.th>
2023-02-27 00:59:25 +0700

    func register but not have admin

--- src/api/activity-registation/controllers/activity-registation.ts
+++ src/api/activity-registation/controllers/activity-registation.ts
@@ -3,5 +3,32 @@
  */
 
 import { factories } from '@strapi/strapi'
+import {parseMultipartData} from '@strapi/utils'
 
-export default factories.createCoreController('api::activity-registation.activity-registation');
+export default factories.createCoreController('api::activity-registation.activity-registation' ,({strapi}) =>({async update(ctx) {
+    const entityId = ctx.params.id;
+    let entity;
+    
+    const actre = await strapi.entityService.findOne('api::activity-registation.activity-registation',entityId,{
+        populate: {Register:true},
+    });
+    
+    if (!actre){
+        return ctx.notFound(`Not Found`);
+    }
+    if(actre.Register?.id!==ctx.state.user.id){
+        return ctx.unauthorized(`You can't update this entry`);
+    }
+    
+    if(ctx.is(`multipart`)){
+                             
+        const {data,files} = parseMultipartData(ctx);
+        entity = await strapi.entityService.update('api::activity-registation.activity-registation',entityId,{data},{files});
+    } 
+    else{
+        entity = await strapi.entityService.update('api::activity-registation.activity-registation',entityId,ctx.request.body);
+    }
+    const sanitizedEntity = await this.sanitizeOutput(entity,ctx);
+    return this.transformResponse(sanitizedEntity);
+},
+}));
